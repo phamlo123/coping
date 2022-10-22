8452798050c6d97d73df1fe8066d5b081d3b7ece
phamlo123 <pham.lo@northeastern.edu>
2022-10-22 19:32:14 -0400

    ,

--- backend/coping/settings.py
+++ backend/coping/settings.py
@@ -68,9 +68,9 @@ MIDDLEWARE = [
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build/static')
+    os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
@@ -164,7 +164,7 @@ CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
 )
 
-BASE_DIR = Path(__file__).resolve().root.root
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 dotenv_file = os.path.join(BASE_DIR, ".env")
 if os.path.isfile(dotenv_file):
